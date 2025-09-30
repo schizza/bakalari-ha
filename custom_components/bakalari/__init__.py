76c7ab6251585d1fedfcf374c760543278aa0fdd
schizza <opensource@schizza.cz>
2025-09-30 13:22:09 +0200

    fix: add config schema validation.

--- custom_components/bakalari/__init__.py
+++ custom_components/bakalari/__init__.py
@@ -4,8 +4,11 @@ from __future__ import annotations
 
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers import config_validation as cv
 
 from .const import DOMAIN, PLATFORMS
+
+CONFIG_SCHEMA = cv.config_entry_only_config_schema(DOMAIN)
 
 
 async def async_setup(hass: HomeAssistant, config) -> bool:
